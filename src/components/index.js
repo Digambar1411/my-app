a92199334430e2c40e2901f12ec24294b0448352
Digambar <deshawaldigambar@gmail.com>
2023-01-07 20:54:45 +0530

    feat : barrel import, export of pages & components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import { Aside } from "./Aside/Aside";
+import { Nav } from "./Nav/Nav";
+import { Chatbox } from "./ChatBox/Chatbox";
+import { Address } from "./Address/Address";
+import { AccountModal } from "./AccountModal/AccountModal";
+import { CommingSoon } from "./Comming Soon/CommingSoon";
+
+export { Aside, Nav, Chatbox, Address, AccountModal,CommingSoon}
