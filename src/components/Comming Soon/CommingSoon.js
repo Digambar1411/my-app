bdc86c3c3b94c9ff5e6dff61f36ba6edcc76405a
Digambar <deshawaldigambar@gmail.com>
2023-01-07 20:52:48 +0530

    feat : added few components

---
+++ src/components/Comming Soon/CommingSoon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./CommingSoon.css"
+
+function CommingSoon() {
+  return (
+    <div className='comming-soon-template'>
+        Comming Soon
+    </div>
+  )
+}
+
+export  {CommingSoon}
