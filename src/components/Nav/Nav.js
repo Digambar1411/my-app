2261ebf909918513b080fc70d89b37a8f1095c26
Digambar <deshawaldigambar@gmail.com>
2023-01-07 22:45:30 +0530

    feat : added navbar component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,46 @@
+import React, {  useState } from "react";
+import "./Nav.css";
+import { useUsers } from "../../contexts/userContext";
+import { useParams, useLocation } from "react-router-dom";
+import { AccountModal } from "../AccountModal/AccountModal";
+
+function Nav() {
+	const { id } = useParams();
+	const { currentUser} = useUsers();
+	const { pathname } = useLocation();
+	const [accountModal, setAccountModal] = useState(false);
+
+	const showAccountModal = () => {
+		setAccountModal(true);
+	};
+
+	return (
+		<div className="nav">
+			<span className="page-title"> {pathname.toString().split("/")[3]}</span>
+			{currentUser.map((user) => {
+				return (
+					<div
+						key={user.id}
+						className="flex-row flex-center user-account"
+						onClick={showAccountModal}
+					>
+						<img
+							className="user-profile-pic-sm"
+							src={`${user.profilepicture}`}
+							alt="small-profile-icon"
+						/>
+						<p className="user-name">{user.name}</p>
+					</div>
+				);
+			})}
+			{accountModal && (
+				<AccountModal
+					setAccountModal={setAccountModal}
+					currentUser={currentUser}
+				/>
+			)}
+		</div>
+	);
+}
+
+export { Nav };
