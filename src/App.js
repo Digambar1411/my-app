84c687891d3a20526ae829c7ea1d9835b6376628
Digambar <deshawaldigambar@gmail.com>
2023-01-07 20:49:59 +0530

    feat : Added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Landing, Home, Todo, Posts, Gallery } from "./pages";
 
 function App() {
-  return (
-    <div className="App">
-     Test
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Landing />} />
+				<Route path="/screen/:id/profile" element={<Home />} />
+				<Route path="/screen/:id/gallery" element={<Gallery />} />
+				<Route path="/screen/:id/post" element={<Posts />} />
+				<Route path="/screen/:id/todo" element={<Todo />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
