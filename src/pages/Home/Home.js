dbff581601928a93baf8ab540d3c569ade4d2568
Digambar <deshawaldigambar@gmail.com>
2023-01-08 12:28:50 +0530

    home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,44 +13,46 @@ function Home() {
 				<div className="right-section">
 					<Nav />
 					<div className="main-container">
-						<div className="main-left">
-							{currentUser.map((user) => {
-								return (
-									<>
-										<img
-											className="profile-pic-large"
-											src={user.profilepicture}
-										/>
-										<strong className="user-name center">{user.name}</strong>
-										<div>
-											<span>Username</span> : <strong> {user.username}</strong>
-										</div>
-										<div>
-											<span>e-mail</span> : <strong>{user.email}</strong>
-										</div>
-										<div>
-											<span>Phone</span> : <strong>{user.phone.split(" ")[0].split("-").join("")}</strong>
-										</div>
-										<div>
-											<span>Website</span> : <strong>{user.website}</strong>
-										</div>
-										<span className="center company">Company</span>
-										<div>
-											<span>Name</span> : <strong>{user.company.name}</strong>
-										</div>
-										<div>
-											<span>catchphrase</span> :{" "}
-											<strong>{user.company.catchPhrase}</strong>
-										</div>
-										<div>
-											<span>bs </span> : <strong>{user.company.bs}</strong>
-										</div>
-									</>
-								);
-							})}
-						</div>
+						{currentUser.map((user) => {
+							return (
+								<div key={user.id} className="main-left">
+									<img
+										className="profile-pic-large"
+										src={user.profilepicture}
+									/>
+									<strong className="user-name center">{user.name}</strong>
+									<div>
+										<span>Username</span> : <strong> {user.username}</strong>
+									</div>
+									<div>
+										<span>e-mail</span> : <strong>{user.email}</strong>
+									</div>
+									<div>
+										<span>Phone</span> :{" "}
+										<strong>
+											{user.phone.split(" ")[0].split("-").join("")}
+										</strong>
+									</div>
+									<div>
+										<span>Website</span> : <strong>{user.website}</strong>
+									</div>
+									<span className="center company">Company</span>
+									<div>
+										<span>Name</span> : <strong>{user.company.name}</strong>
+									</div>
+									<div>
+										<span>catchphrase</span> :{" "}
+										<strong>{user.company.catchPhrase}</strong>
+									</div>
+									<div>
+										<span>bs </span> : <strong>{user.company.bs}</strong>
+									</div>
+								</div>
+							);
+						})}
+
 						<div className="main-right">
-							<Address selectedUser={currentUser}/>
+							<Address selectedUser={currentUser} />
 						</div>
 					</div>
 				</div>
